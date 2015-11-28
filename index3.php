4895ff4c8ba0bbd9e508f74d65bc88b239d3c30a
esraerik <esraerik1@gmail.com>
2015-11-28 17:04:30 +0200

    v1
    smrating heroku

---
+++ index3.php
@@ -0,0 +1,17 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<meta charset="utf-8">
+<meta http-equiv="X-UA-Compatible" content="IE-edge">
+<title>home</title>
+</head>
+<body>
+<div id="content" style="width: 70%; float:left;">
+<h2>Welcome</h2>
+</div>
+<div id="sidebar" style="width:20%; float:right">
+Side bar content
+</div>
+</body>
+
+</html>
